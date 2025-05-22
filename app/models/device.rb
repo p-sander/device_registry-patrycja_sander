964980e924cb876636bc8abf8e5c72e9122cbdd2
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-22 15:10:08 +0200

    added DeviceAssignment model with User and Device relationships
    - added DeviceAssignment with 'belongs to' relationship to User and Device
    - updated User model to reflect new relationship
    - updated Device model to reflect new relationship
    - implemented validations (can't assign multiple users to one device assignment now)

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,4 @@
 class Device < ApplicationRecord
+  has_one :device_assignment, -> { where(returned: false) }, class_name: "DeviceAssignment"
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :devices
+  has_many :device_assignments, dependent: :destroy
+  has_many :devices, through: :device_assignments
   has_many :api_keys, as: :bearer
   has_secure_password
+
 end
