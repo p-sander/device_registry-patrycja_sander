8fc08ce4fe14f39185b7e4e54219e4e0133d6176
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 20:07:41 +0200

    Enhance ReturnDeviceFromUser class with input validation and improved error handling
    - Added `validate_inputs!` method to check for missing user or serial number inputs.
    - Introduced `attr_reader` for `user` and `serial_number` to improve accessibility.
    - Updated `call` method to validate inputs before proceeding with device and assignment lookup.
    - Improved error handling by raising `ArgumentError` for invalid inputs.

--- app/services/return_device_from_user.rb
+++ app/services/return_device_from_user.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 class ReturnDeviceFromUser
+  attr_reader :user, :serial_number
   def initialize(user:, serial_number:)
     @user = user
     @serial_number = serial_number
   end
 
   def call
-    device = find_device(@serial_number)
+    validate_inputs!
+    device = find_device(serial_number)
+    device_assignment = find_device_assignment(user, device)
   end
 
   private
+
+  def validate_inputs!
+    raise ArgumentError, "No user" if user.nil?
+    raise ArgumentError, "No serial number" if serial_number.blank?
+  end
 
   def find_device(serial_number)
     Device.find_by(serial_number) ||
@@ -20,7 +28,6 @@ class ReturnDeviceFromUser
   def find_device_assignment(user, device)
     DeviceAssignment.find_by(user, device) ||
       raise(RegistrationError::Unauthorized, "You cannot return a device you did not assign.")
-
   end
 end
 
