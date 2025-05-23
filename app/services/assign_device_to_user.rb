a212f7d2540dc3812c452a404e91c79cfcb32324
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 12:22:06 +0200

    add private method validate_input! that validates arguments given in the initializer

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -2,6 +2,7 @@
 
 class AssignDeviceToUser
   attr_reader :requesting_user, :serial_number, :new_device_owner_id
+
   def initialize(requesting_user:, serial_number:, new_device_owner_id:)
     @requesting_user = requesting_user
     @serial_number = serial_number
@@ -9,6 +10,15 @@ class AssignDeviceToUser
   end
 
   def call
+    validate_inputs!
   end
 
+
+  private
+
+  def validate_inputs!
+    raise ArgumentError, "No user" if requesting_user.nil?
+    raise ArgumentError, "No serial number" if serial_number.nil?
+    raise ArgumentError, "No id of the owner" if new_device_owner_id.zero?
+  end
 end
