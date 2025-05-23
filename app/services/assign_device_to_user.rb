af98127d27f49ded155aae25111e692c5d78515c
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 12:52:50 +0200

    Add private methods: find_new_device_owner and compare_requesting_user_and_new_device_owner
    - find_new_device_owner: finds user by ID from initializer
    -compare_requesting_user_and_new_device_owner: compares two users for equality

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -11,8 +11,9 @@ class AssignDeviceToUser
 
   def call
     validate_inputs!
+    future_device_owner = find_new_device_owner
+    compare_requesting_user_and_new_device_owner(future_device_owner)
   end
-
 
   private
 
@@ -20,5 +21,16 @@ class AssignDeviceToUser
     raise ArgumentError, "No user" if requesting_user.nil?
     raise ArgumentError, "No serial number" if serial_number.nil?
     raise ArgumentError, "No id of the owner" if new_device_owner_id.zero?
+  end
+
+  def find_new_device_owner
+    User.find_by(id: new_device_owner_id) ||
+      raise(ActiveRecord::RecordNotFound, "No user with id #{new_device_owner_id}")
+  end
+
+  def compare_requesting_user_and_new_device_owner(new_device_owner)
+    unless requesting_user == new_device_owner
+      raise ArgumentError, "You can't assign a device to someone else, just to yourself"
+    end
   end
 end
