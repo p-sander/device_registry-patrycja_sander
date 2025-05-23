effebcacfb0376268b617b60a3bcccb32be715b0
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 10:33:30 +0200

    methods added to class service AssignDeviceToUser - add initializer - add attr_reader

--- app/services/assign_device_to_user.rb
+++ app/services/assign_device_to_user.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
 class AssignDeviceToUser
+  attr_reader :requesting_user, :serial_number, :new_device_owner_id
   def initialize(requesting_user:, serial_number:, new_device_owner_id:)
+    @requesting_user = requesting_user
+    @serial_number = serial_number
+    @new_device_owner_id = new_device_owner_id.to_i
   end
 
   def call
   end
+
 end
