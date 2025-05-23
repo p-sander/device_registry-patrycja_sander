2d23cfd6380389b78293af830d01824fff5ccdac
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 23:35:26 +0200

    Fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  scope '/api' do
+  namespace :api do
     post :assign, to: 'devices#assign'
     post :unassign, to: 'devices#unassign'
   end
