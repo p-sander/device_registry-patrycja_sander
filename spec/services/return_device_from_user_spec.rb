f2667875188641821a356c2439571f4336037873
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 19:16:52 +0200

    Add test for successful device return
    - Implemented test to ensure that a device can be successfully returned by the user who assigned it.
    - Device assignment status is updated to `returned` and `returned_at` is set.

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -13,6 +13,19 @@ RSpec.describe ReturnDeviceFromUser do
   let(:user) { create(:user) }
   let(:device) { create(:device, serial_number: '123456') }
 
+  context 'when user returns a device assigned to themself' do
+    before do
+      AssignDeviceToUser.new(
+        requesting_user: user,
+        serial_number: device.serial_number,
+        new_device_owner_id: user.id
+      ).call
+    end
 
+    it 'successfully returns device' do
+      expect(return_device).not_to raise_error
+      expect(DeviceAssignment.find_by(user: user, device: device).returned).to eq(true)
+    end
+  end
 
 end
