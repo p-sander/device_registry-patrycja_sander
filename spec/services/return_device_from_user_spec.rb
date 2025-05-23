60e40e12b2257b9b793084c2848da1a89260cf5c
Patrycja Sander <patrycja.sander2@gmail.com>
2025-05-23 19:16:21 +0200

    Enhance RSpec test file for ReturnDeviceFromUser service
    - Set up the basic structure for the tests.
    - Define `user` and `device` factories for use in later tests.

--- spec/services/return_device_from_user_spec.rb
+++ spec/services/return_device_from_user_spec.rb
@@ -3,5 +3,16 @@
 require 'rails_helper'
 
 RSpec.describe ReturnDeviceFromUser do
-  # TODO: Implement the tests for ReturnDeviceFromUser
+  subject(:return_device) do
+    described_class.new(
+      user: user,
+      serial_number: device.serial_number
+    ).call
+
+  end
+  let(:user) { create(:user) }
+  let(:device) { create(:device, serial_number: '123456') }
+
+
+
 end
